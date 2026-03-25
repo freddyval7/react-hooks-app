7b3535b45ebb84c275a28527a3ef20338cec4619
Freddy Tomada <freddytomada7@gmail.com>
2026-03-25 15:55:54 -0400

    add z validator

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { TasksApp } from "./05-useReducer/TaskApp";
 // import { HooksApp } from "./HooksApp.tsx";
 // import { TrafficLight } from "./01-useState/TrafficLight.tsx";
 // import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect.tsx";
 // import { PokemonPage } from "./03-examples/PokemonPage.tsx";
 // import { FocusScreen } from "./04-useRef/FocusScreen.tsx";
+// import { TasksApp } from "./05-useReducer/TaskApp";
+import { ScrambleWords } from "./05-useReducer/ScrambleWords";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -16,6 +17,7 @@ createRoot(document.getElementById("root")!).render(
     {/* <TrafficLightWithHook /> */}
     {/* <PokemonPage /> */}
     {/* <FocusScreen /> */}
-    <TasksApp />
+    {/* <TasksApp /> */}
+    <ScrambleWords />
   </StrictMode>,
 );
