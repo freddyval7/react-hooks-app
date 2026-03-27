a466a89833f33c7d2efe9b4342748e332041ecd5
Freddy Tomada <freddytomada7@gmail.com>
2026-03-26 21:59:44 -0400

    add useOptimistic, useTransition and use API

---
+++ src/08-use-suspense/ClientInformation.tsx
@@ -0,0 +1,24 @@
+import { use, type Usable } from "react";
+import { type User } from "./api/get-user.action";
+
+interface Props {
+  getUser: Usable<User>;
+}
+
+const ClientInformation = ({ getUser }: Props) => {
+  const user = use(getUser);
+
+  return (
+    <div className="bg-gradient fle flex-col gap-4">
+      <h2 className="text-4xl font-thin text-white">
+        {user.name} - #{user.id}
+      </h2>
+
+      <p className="text-white text-2xl">{user.location}</p>
+
+      <p className="text-white text-2xl">{user.role}</p>
+    </div>
+  );
+};
+
+export default ClientInformation;
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,24 @@
-import { StrictMode } from "react";
+import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+
+import { Toaster } from "sonner";
+
 // import { HooksApp } from "./HooksApp.tsx";
 // import { TrafficLight } from "./01-useState/TrafficLight.tsx";
 // import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect.tsx";
 // import { PokemonPage } from "./03-examples/PokemonPage.tsx";
 // import { FocusScreen } from "./04-useRef/FocusScreen.tsx";
 // import { TasksApp } from "./05-useReducer/TaskApp";
-import { ScrambleWords } from "./05-useReducer/ScrambleWords";
+// import { ScrambleWords } from "./05-useReducer/ScrambleWords";
+// import { MemoHook } from "./06-memos/MemoHook";
+// import { InstagromApp } from "./07-useOptimistic/InstagromApp";
+import ClientInformation from "./08-use-suspense/ClientInformation";
+import { getUserAction } from "./08-use-suspense/api/get-user.action";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Toaster />
     {/* <HooksApp /> */}
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
@@ -18,6 +26,17 @@ createRoot(document.getElementById("root")!).render(
     {/* <PokemonPage /> */}
     {/* <FocusScreen /> */}
     {/* <TasksApp /> */}
-    <ScrambleWords />
+    {/* <ScrambleWords /> */}
+    {/* <MemoHook /> */}
+    {/* <InstagromApp /> */}
+    <Suspense
+      fallback={
+        <div className="bg-gradient">
+          <h1 className="text-2xl">Cargando...</h1>
+        </div>
+      }
+    >
+      <ClientInformation getUser={getUserAction(1000)} />
+    </Suspense>
   </StrictMode>,
 );
